64143083aacf4ee3e2881838626bd1f80295c77a
SANJAY S <79918985+sanjay1842@users.noreply.github.com>
2022-11-05 14:07:18 +0530

    Update BubbleSort.java

--- BubbleSort.java
+++ BubbleSort.java
@@ -1,3 +1,4 @@
+//compile-time
 import java.util.*;
 public class Main
 {
@@ -19,6 +20,39 @@ public class Main
 	    Main obj=new Main();
 		Scanner sc=new Scanner(System.in);
 		int[] arr={4,8,7,9,2,5,1};
+		
+		int result=obj.bubbleSort(arr);
+		System.out.println("sorted array is"+Arrays.toString(arr));
+		
+}
+}
+//runtime
+import java.util.*;
+public class Main
+{
+    int bubbleSort(int[] array){
+        int x=array.length;
+        for(int i=0;i<x-1;i++){
+            for(int j=0;j<x-i-1;j++){
+                if(array[j]>array[j+1]){
+                    int temp=array[j];
+                    array[j]=array[j+1];
+                    array[j+1]=temp;
+                }
+            }
+        }
+        return 0;
+        
+    }
+	public static void main(String[] args) {
+	    Main obj=new Main();
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int[] arr=new int[n];
+		
+		for(int i=0;i<n;i++){
+		    arr[i]=sc.nextInt();
+		}
 		
 		int result=obj.bubbleSort(arr);
 		System.out.println("sorted array is"+Arrays.toString(arr));
